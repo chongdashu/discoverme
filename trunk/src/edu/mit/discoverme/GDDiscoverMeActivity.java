cc7dd4a5b8029e4ffb092026d36697834d6217df
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-12 02:44:36 +0000

    updated project.

--- trunk/src/edu/mit/discoverme/GDDiscoverMeActivity.java
+++ trunk/src/edu/mit/discoverme/GDDiscoverMeActivity.java
@@ -1,7 +1,11 @@
 package edu.mit.discoverme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class GDDiscoverMeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,21 @@ public class GDDiscoverMeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button button = (Button) (findViewById(R.id.button1));
+        button.setText(R.string.button_text_friend);
+        
+        button.setOnClickListener(onButton1Click);
     }
+    
+    private OnClickListener onButton1Click = new OnClickListener() {
+        public void onClick(View v) {
+          // do something when the button is clicked
+        	
+        	Intent intent = new Intent(GDDiscoverMeActivity.this, EventsActivity.class);
+        	startActivity(intent);
+        }
+    };
+    
+    
 }
