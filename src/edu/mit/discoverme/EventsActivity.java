01d6f6eedd897f8fa3ce125c824a1efe4d4e900f
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-12 02:44:36 +0000

    updated project.

---
+++ src/edu/mit/discoverme/EventsActivity.java
@@ -0,0 +1,15 @@
+package edu.mit.discoverme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.events);
+	}
+	
+
+}
