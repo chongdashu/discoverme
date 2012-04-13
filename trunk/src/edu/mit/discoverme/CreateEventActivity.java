38d8b914f71c9d0ce2167ad4e2e9858b08281515
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-13 21:29:14 +0000

    Create Event Activity WIP.

---
+++ trunk/src/edu/mit/discoverme/CreateEventActivity.java
@@ -0,0 +1,14 @@
+package edu.mit.discoverme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateEventActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.create_event);
+	}
+
+}
