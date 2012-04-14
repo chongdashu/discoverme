3073353c51bfeb842c405230c94e6c916e8d47d3
sunilasaqib1@gmail.com <sunilasaqib1@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-14 01:23:31 +0000

    fixing activity-transitions and integrated create events activity.

--- trunk/src/edu/mit/discoverme/PopupListActivity.java
+++ trunk/src/edu/mit/discoverme/PopupListActivity.java
@@ -16,13 +16,14 @@ import android.widget.Toast;
 
 public class PopupListActivity extends ListActivity {
 	
+	String popup;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.popup_list);
 
 		ImageButton friend = (ImageButton) (findViewById(R.id.friendButton));
-		// button.setText(R.string.button_text_friend);
 		friend.setOnClickListener(onFriendClick);
 
 		ImageButton event = (ImageButton) (findViewById(R.id.eventButton));
@@ -31,10 +32,14 @@ public class PopupListActivity extends ListActivity {
 		ImageButton notif = (ImageButton) (findViewById(R.id.notificationButton));
 		notif.setOnClickListener(onNotificationClick);
 
+		
 		Intent intent = getIntent();
-		String popup = intent.getStringExtra("popupCode");// savedInstanceState.getInt("popupCode");
+		popup = intent.getStringExtra("popupCode");// savedInstanceState.getInt("popupCode");
 
 		Button b = (Button) findViewById(R.id.buttonAdd);
+
+		b.setOnClickListener(onAddClick);
+
 		if (popup.equals("friendss")) {
 			String[] friends = getResources().getStringArray(
 					R.array.friends_array);
@@ -78,10 +83,18 @@ public class PopupListActivity extends ListActivity {
 		public void onClick(View v) {
 			// do something when the button is clicked
 
-			Intent intent = new Intent(PopupListActivity.this,
+			if (popup.equals("friendss")) {
+				finishActivity(-1);
+			} else {
+				Intent intent = new Intent(PopupListActivity.this,
 					PopupListActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			intent.putExtra("popupCode", "friendss");
-			startActivity(intent);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+
+				finishActivity(-1);
+			}
 
 		}
 	};
@@ -90,10 +103,17 @@ public class PopupListActivity extends ListActivity {
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			Intent intent = new Intent(PopupListActivity.this,
+			if (popup.equals("eventss")) {
+				finishActivity(-1);
+			} else {
+
+				Intent intent = new Intent(PopupListActivity.this,
 					PopupListActivity.class);// EventsActivity.class);
 			intent.putExtra("popupCode", "eventss");
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			startActivity(intent);
+				finishActivity(-1);
+			}
 		}
 	};
 
@@ -102,10 +122,40 @@ public class PopupListActivity extends ListActivity {
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
+			if (popup.equals("notifss")) {
+				finishActivity(-1);
+			} else {
 			Intent intent = new Intent(PopupListActivity.this,
 					PopupListActivity.class);// NotificationsActivity.class);
 			intent.putExtra("popupCode", "notifss");
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			startActivity(intent);
+				finishActivity(-1);
+			}
+		}
+	};
+
+	private final OnClickListener onAddClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			if (popup.equals("friendss")) {
+				// it should go to search page
+				Intent intent = new Intent(PopupListActivity.this,
+						CreateEventActivity.class);// NotificationsActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+				finishActivity(-1);
+			}
+			if (popup.equals("eventss")) {
+				Intent intent = new Intent(PopupListActivity.this,
+						CreateEventActivity.class);// NotificationsActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+				finishActivity(-1);
+			}
+
 		}
 	};
 
