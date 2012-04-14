934d0f8cc23e97a52edbca8c81c9c29ec4b4e422
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-14 04:43:24 +0000

    * Tapping on "Select Participants" now no longer shows keyboard. Launches "Select Participant" Activity instead.  * Tapping on "Select Location" now no longer shows keyboard. Launches "Select Location from Map" Activity instead.

--- src/edu/mit/discoverme/CreateEventActivity.java
+++ src/edu/mit/discoverme/CreateEventActivity.java
@@ -1,14 +1,52 @@
 package edu.mit.discoverme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class CreateEventActivity extends Activity {
+
+	private EditText editTextParticipants;
+	private EditText editTextLocation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_event);
+
+		// Set up listeners for "Selecting Participants"
+		EditText editTextParticipants = (EditText) (findViewById(R.id.create_event_edittext_participants));
+		editTextParticipants.setOnClickListener(onEditTextParticipantsClick);
+
+		// Set up listeners for "Selecting Location"
+		EditText editTextLocation = (EditText) (findViewById(R.id.create_event_edittext_location));
+		editTextLocation.setOnClickListener(onEditTextLocationClick);
 	}
+
+	private OnClickListener onEditTextParticipantsClick = new OnClickListener() {
+		public void onClick(View v) {
+			// Do something when "Select Participants" is clicked.
+
+			// We launch the "Select Participants Activity"
+			Intent intent = new Intent(CreateEventActivity.this, ParticipantListingActivity.class);
+			//intent.putExtra("popupCode", "eventss");
+			startActivity(intent);
+		}
+	};
+
+	private OnClickListener onEditTextLocationClick = new OnClickListener() {
+		public void onClick(View v) {
+			// Do something when "Select Location" is clicked.
+
+			// We launch the "Select Location from Map Activity"
+			Intent intent = new Intent(CreateEventActivity.this, MapTestActivity.class);
+			//intent.putExtra("popupCode", "eventss");
+			startActivity(intent);
+
+		}
+	};
 
 }
