b99e06fbbafa2fc5c5963e9a00566c9d786933dc
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-14 20:59:50 +0000

    * Add markers on tapping screen.

--- trunk/src/edu/mit/discoverme/HelloItemizedOverlay.java
+++ trunk/src/edu/mit/discoverme/HelloItemizedOverlay.java
@@ -15,7 +15,7 @@ public class HelloItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	private Context mContext;
-	
+
 	public HelloItemizedOverlay(Drawable defaultMarker, Context context) {
 		super(boundCenterBottom(defaultMarker));
 		mContext = context;
@@ -23,12 +23,39 @@ public class HelloItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 
 	@Override
 	public boolean onTap(int index) {
-		OverlayItem item = mOverlays.get(index);
-		  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
-		  dialog.setTitle(item.getTitle());
-		  dialog.setMessage(item.getSnippet());
-		  dialog.show();
-		  return true;
+		boolean tapped = super.onTap(index);
+		if (tapped) {
+			OverlayItem item = mOverlays.get(index);
+			AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+			dialog.setTitle(item.getTitle());
+			dialog.setMessage(item.getSnippet());
+			dialog.show();
+			
+		} else {
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onTap(final GeoPoint p, final MapView mapView) {
+		boolean tapped = super.onTap(p, mapView);
+		if (tapped) {
+			// do what you want to do when you hit an item
+		} else {
+			 Drawable drawable = mapView.getContext().getResources().getDrawable(R.drawable.androidmarker);
+			 OverlayItem overlayitem = new OverlayItem(p, "Chong-U", "Chong-U Lim");
+			 addOverlay(overlayitem);
+		}
+		return true;
+	}
+
+	private OverlayItem drawMarker(GeoPoint p) {
+		OverlayItem overlayitem = new OverlayItem(p,
+				"Select As Game Location?",
+				"Do you want this location to be added as the location for the game?");
+		mOverlays.clear();
+		addOverlay(overlayitem);
+		return overlayitem;
 	}
 
 	@Override
@@ -40,10 +67,10 @@ public class HelloItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 	public int size() {
 		return mOverlays.size();
 	}
-	
+
 	public void addOverlay(OverlayItem overlay) {
-	    mOverlays.add(overlay);
-	    populate();
+		mOverlays.add(overlay);
+		populate();
 	}
 
 }
