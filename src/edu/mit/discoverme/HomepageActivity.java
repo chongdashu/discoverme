fbd9ba10cccd5df9f8734fe147766e866488e94a
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-15 15:23:37 +0000

    * Made HomePage's popups fill_parent.  * Updated the photo to include a map background.

--- src/edu/mit/discoverme/HomepageActivity.java
+++ src/edu/mit/discoverme/HomepageActivity.java
@@ -16,6 +16,12 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -191,6 +197,8 @@ public class HomepageActivity extends MapActivity {
 		RelativeLayout popupArea = (RelativeLayout) (findViewById(R.id.popup_area));
 		popupArea.setVisibility(View.VISIBLE);
 		fillPopup();
+		// setLayoutAnim_slidedown(popupArea, this);
+		
 
 	}
 
@@ -320,6 +328,88 @@ public class HomepageActivity extends MapActivity {
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public void setLayoutAnim_slidedown(ViewGroup panel, Context ctx) {
+		 
+		AnimationSet set = new AnimationSet(true);
+ 
+		Animation animation = new TranslateAnimation(
+				Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+				0.0f, Animation.RELATIVE_TO_SELF, -1.0f,
+				Animation.RELATIVE_TO_SELF, 0.0f);
+		animation.setDuration(800);
+		animation.setAnimationListener(new AnimationListener() {
+ 
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+				// MapContacts.this.mapviewgroup.setVisibility(View.VISIBLE);
+ 
+			}
+ 
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+ 
+			}
+ 
+			@Override
+			public void onAnimationEnd(Animation animation) {
+ 
+				// TODO Auto-generated method stub
+ 
+			}
+		});
+		set.addAnimation(animation);
+ 
+		LayoutAnimationController controller = new LayoutAnimationController(
+				set, 0.25f);
+		panel.setLayoutAnimation(controller);
+ 
+	}
+ 
+	public void setLayoutAnim_slideup(ViewGroup panel, Context ctx) {
+ 
+		AnimationSet set = new AnimationSet(true);
+ 
+		/*
+		 * Animation animation = new AlphaAnimation(1.0f, 0.0f);
+		 * animation.setDuration(200); set.addAnimation(animation);
+		 */
+ 
+		Animation animation = new TranslateAnimation(
+				Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+				0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+				Animation.RELATIVE_TO_SELF, -1.0f);
+		animation.setDuration(800);
+		animation.setAnimationListener(new AnimationListener() {
+ 
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+ 
+			}
+ 
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+ 
+			}
+ 
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// MapContacts.this.mapviewgroup.setVisibility(View.INVISIBLE);
+				// TODO Auto-generated method stub
+ 
+			}
+		});
+		set.addAnimation(animation);
+ 
+		LayoutAnimationController controller = new LayoutAnimationController(
+				set, 0.25f);
+		panel.setLayoutAnimation(controller);
+ 
 	}
 
 }
