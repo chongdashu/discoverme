6a156c605ecc46d7ae952d7a2667bed5d359345f
sunilasaqib1@gmail.com <sunilasaqib1@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-15 03:09:15 +0000

    completed select participants.

--- src/edu/mit/discoverme/ParticipantListingActivity.java
+++ src/edu/mit/discoverme/ParticipantListingActivity.java
@@ -1,71 +1,53 @@
 package edu.mit.discoverme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ParticipantListingActivity extends Activity {
 
-	static final String[] COUNTRIES = new String[] {
-	    "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-	    "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-	    "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-	    "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-	    "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-	    "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-	    "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-	    "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-	    "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
-	    "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
-	    "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-	    "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-	    "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-	    "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-	    "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-	    "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-	    "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-	    "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-	    "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-	    "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-	    "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-	    "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-	    "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
-	    "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-	    "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-	    "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-	    "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-	    "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-	    "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-	    "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-	    "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-	    "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-	    "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-	    "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-	    "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-	    "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-	    "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-	    "Ukraine", "United Arab Emirates", "United Kingdom",
-	    "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-	    "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
-	    "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
-	  };
+
+	String selectedList;
+	String[] friends;
+	int[] selected;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.participant_list);
+		selectedList = "";
+
+		Button back = (Button) (findViewById(R.id.backButton));
+		back.setText("Cancel");
+		back.setOnClickListener(onCancelClick);
+
+		Button next = (Button) (findViewById(R.id.nextButton));
+		next.setText("Done");
+		next.setOnClickListener(onDoneClick);
+
+		friends = getResources().getStringArray(R.array.friends_array);
+
+		selected = new int[friends.length];
 
 		ListView l = (ListView) findViewById(R.id.participant_listview);
 		l.setBackgroundColor(Color.WHITE);
 		l.setCacheColorHint(Color.WHITE);
-		l.setAdapter(new ArrayAdapter<String>(this, R.layout.participant_row, COUNTRIES));
+		l.setAdapter(new ArrayAdapter<String>(this, R.layout.participant_row,
+				friends));
 		l.setOnItemClickListener(new OnItemClickListener() {
+			@Override
 			public void onItemClick(AdapterView arg0, View v, int arg2,
 					long arg3) {
 				CheckedTextView tt = (CheckedTextView) v
@@ -73,13 +55,71 @@ public class ParticipantListingActivity extends Activity {
 				if (!tt.isChecked()) {
 					tt.setChecked(true);
 					tt.setCheckMarkDrawable(android.R.drawable.checkbox_on_background);
+					selected[arg2] = 1;
+
 				} else {
 					tt.setChecked(false);
 					tt.setCheckMarkDrawable(android.R.drawable.checkbox_off_background);
+					selected[arg2] = 0;
 				}
 
 			}
 		});
+	}
+
+	private final OnClickListener onDoneClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			// go back to last page
+			// and flash a message on screen saying friend added
+
+			getSelected();
+			Toast.makeText(getApplicationContext(), selectedList,
+					Toast.LENGTH_SHORT).show();
+			// selectedList = "sunila is there !";
+			Intent resultIntent = new Intent();
+			resultIntent.putExtra("participants", selectedList);
+			Activity partSelActivity = ParticipantListingActivity.this;
+			partSelActivity.setResult(Activity.RESULT_OK, resultIntent);
+			partSelActivity.finish();
+
+		}
+	};
+
+	private final OnClickListener onCancelClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			// go back to last page
+			// and flash a message on screen saying friend added
+			Toast.makeText(getApplicationContext(),
+					getString(R.string.addAsFriendMesg), Toast.LENGTH_SHORT)
+					.show();
+			selectedList = "";
+			Intent resultIntent = new Intent();
+			resultIntent.putExtra("participants", selectedList);
+			Activity partSelActivity = ParticipantListingActivity.this;
+			partSelActivity.setResult(Activity.RESULT_CANCELED, resultIntent);
+			partSelActivity.finish();
+
+		}
+	};
+
+	private final void getSelected()
+	{
+		if (selected != null && friends != null) {
+			selectedList = "";
+			for (int i = 0; i < selected.length; i++) {
+				if (selected[i] == 1)
+					selectedList = selectedList + friends[i] + ", ";
+
+			}
+		}
+ else
+			selectedList = "84038y";
 	}
 
 }
