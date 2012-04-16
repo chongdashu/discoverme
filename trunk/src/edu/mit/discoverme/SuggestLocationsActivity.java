dba7ff9158806a80ba28e6c14dbda1b2df60bff6
sunilasaqib1@gmail.com <sunilasaqib1@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-16 03:58:43 +0000

    segferthwrhjy

--- trunk/src/edu/mit/discoverme/SuggestLocationsActivity.java
+++ trunk/src/edu/mit/discoverme/SuggestLocationsActivity.java
@@ -3,15 +3,18 @@ package edu.mit.discoverme;
 import java.util.Arrays;
 import java.util.List;
 
-
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -28,7 +31,14 @@ public class SuggestLocationsActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.suggested_location_list);
-	    	    
+
+		Button back = (Button) (findViewById(R.id.backButton));
+		back.setOnClickListener(onBackClick);
+
+		Button next = (Button) (findViewById(R.id.nextButton));
+		next.setVisibility(View.INVISIBLE);
+		
+
 	    //get the view that we will be using
 	    l = (ListView)findViewById(R.id.location_listview);
 	    
@@ -38,19 +48,38 @@ public class SuggestLocationsActivity extends Activity {
 	    
 	    //adapt it
 	    l.setAdapter(new EventAdapter(this, R.layout.list_item, locationArray));
+	   
+	    l.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				Activity act = SuggestLocationsActivity.this;
+				Intent resultIntent = getIntent();
+				resultIntent.putExtra("LocationName", position);
+
+				act.setResult(Activity.RESULT_OK, resultIntent);
+				act.finish();
+						
+
+				}
+
+			
+		});
 	}
 		
 	
 	private class EventAdapter extends ArrayAdapter<EventLocation> {
 
-        private EventLocation[] items;
+        private final EventLocation[] items;
 
         public EventAdapter(Context context, int textViewResourceId, EventLocation[] items) {
             super(context, textViewResourceId, items);
             this.items = items;
         }
 
-        public View getView(final int position, View view, ViewGroup parent) {
+        @Override
+		public View getView(final int position, View view, ViewGroup parent) {
             LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = vi.inflate(R.layout.list_item, null);
             
@@ -66,7 +95,7 @@ public class SuggestLocationsActivity extends Activity {
             ImageView food = (ImageView) view.findViewById(R.id.hasFoodView);
             ImageView quiet = (ImageView) view.findViewById(R.id.isQuietView);
             ImageView it = (ImageView) view.findViewById(R.id.hasITView);
-            
+           
             
             //if the field is false for any of the locations, disable the notification
             if(!items[position].hasFood){
@@ -83,5 +112,18 @@ public class SuggestLocationsActivity extends Activity {
 
             return view;
         }
-    }
+
+	}
+
+	private final OnClickListener onBackClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			// go back to last page
+			finish();
+
+		}
+	};
+	
 }
