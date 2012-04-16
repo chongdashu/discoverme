4960d6509cb498924695fe7d61e588f0402cc51d
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-04-16 03:23:42 +0000

    * Added colors to lines for viewing RSVPs.

--- trunk/src/edu/mit/discoverme/SelectEventLocationLinesOverlayColor.java
+++ trunk/src/edu/mit/discoverme/SelectEventLocationLinesOverlayColor.java
@@ -10,25 +10,41 @@ import android.graphics.Point;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
 import com.google.android.maps.Projection;
 
-public class SelectEventLocationLinesOverlay extends Overlay {
-	protected GeoPoint target;
-	protected Vector<GeoPoint> sources;
+public class SelectEventLocationLinesOverlayColor
+		extends
+			SelectEventLocationLinesOverlay {
 
-	protected SelectEventLocationLinesOverlay(GeoPoint target, Vector<GeoPoint> sources) {
-		this.target = target;
-		this.sources = sources;
+	public Vector<Integer> colors = new Vector<Integer>();
+
+	protected SelectEventLocationLinesOverlayColor(GeoPoint target,
+			Vector<GeoPoint> sources) {
+		super(target, sources);
+		for (int i = 0; i < sources.size(); i++) {
+			double rand = Math.random();
+
+			if (rand < 0.2) {
+				colors.add(Color.GRAY);
+			} else if (rand >= 0.2 && rand < 0.6) {
+				colors.add(Color.RED);
+			} else if (rand >= 0.6 && rand < 1.6) {
+				colors.add(Color.GREEN);
+			} else {
+				colors.add(Color.GREEN);
+			}
+		}
 	}
+
 	@Override
 	public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
 			long when) {
 		super.draw(canvas, mapView, shadow);
 		Paint paint;
+		int i = 0;
 		for (GeoPoint point : sources) {
 			paint = new Paint();
-			paint.setColor(Color.GRAY);
+			paint.setColor(colors.get(i));
 			paint.setAntiAlias(true);
 			paint.setStyle(Style.STROKE);
 			paint.setStrokeWidth(3);
@@ -38,8 +54,9 @@ public class SelectEventLocationLinesOverlay extends Overlay {
 			projection.toPixels(target, pt1);
 			projection.toPixels(point, pt2);
 			canvas.drawLine(pt1.x, pt1.y, pt2.x, pt2.y, paint);
+			i++;
 		}
-		
+
 		return true;
 	}
 }
