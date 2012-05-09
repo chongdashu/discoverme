8a92ebfc9e3db5de1bccadd5140dfdf51f826a7a
sunilasaqib1@gmail.com <sunilasaqib1@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-05-09 20:59:05 +0000

    dummy server script

---
+++ trunk/server/script.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>descover me</title>
+</head>
+
+<body>
+<?php
+      
+echo "helo";
+
+
+
+?>
+
+hi there
+
+</body>
+</html>
+
