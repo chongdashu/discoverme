cede3e5e984458afd96ea875c372e23f5f93f945
lim.chong.u@gmail.com <lim.chong.u@gmail.com@51bb98dc-b6e1-2909-4870-5ad650bf32ed>
2012-05-12 02:42:24 +0000

    * Enable button upon checking of the credentials.  * Added listeners for the keyboard.

--- src/edu/mit/discoverme/AuthenticationActivity.java
+++ src/edu/mit/discoverme/AuthenticationActivity.java
@@ -5,16 +5,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 public class AuthenticationActivity extends Activity {
 	MyDataSource datasource;
 	DirDataSource dirdatasource;
-
+	Button enterB;
+	EditText password;
+	EditText userName;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,53 +31,113 @@ public class AuthenticationActivity extends Activity {
 		Button cancelB = (Button) (findViewById(R.id.cancel));
 		cancelB.setOnClickListener(onCancelClick);
 
-		Button enterB = (Button) (findViewById(R.id.enter));
+		enterB = (Button) (findViewById(R.id.enter));
 		enterB.setOnClickListener(onEnterClick);
+		enterB.setEnabled(false);
+		
+		password =  (EditText)(findViewById(R.id.password));
+		password.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				String text = s.toString();
+				String username = password.getText().toString();
+				enterB.setEnabled(text.length() > 0 && username.length() > 0);
+			}
+		});
+		password.setOnEditorActionListener(new OnEditorActionListener() {
+			
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if(actionId==EditorInfo.IME_ACTION_DONE){
+					doLogin();
+		        }
+		    return false;
+			}
+		});
+		
+		userName = (EditText)(findViewById(R.id.username));
+		userName.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				String text = s.toString();
+				String pword = password.getText().toString();
+				enterB.setEnabled(text.length() > 0 && pword.length() > 0);
+			}
+		});
 
+	}
+
+	private void doLogin() {
+		TextView username = (TextView) findViewById(R.id.username);
+		TextView password = (TextView) findViewById(R.id.password);
+
+		String usr = username.getText().toString();
+		String pswrd = password.getText().toString();
+
+		Boolean auth = Authenticate.check(usr, pswrd);
+		if (auth) {
+			SharedPreferences prefs = getSharedPreferences("credentials",
+					Context.MODE_WORLD_READABLE);
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.putString("username", usr);
+			editor.putString("password", pswrd);
+			editor.commit();
+			datasource = new MyDataSource(AuthenticationActivity.this);
+			datasource.open();
+			dirdatasource = new DirDataSource(AuthenticationActivity.this);
+			dirdatasource.open();
+			ServerLink.loadPeople(dirdatasource);
+			ServerLink.loadFriends(usr, datasource);
+			ServerLink.loadEvents("saqib01", datasource);
+			// ServerLink.loadEvents("pmerc02", datasource);
+			// ServerLink.loadEvents("pmerc03", datasource);
+			ServerLink.loadNotifs(usr, datasource, dirdatasource);
+			datasource.close();
+			dirdatasource.close();
+			Toast.makeText(getApplicationContext(), "valid credentials!!",
+					Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(AuthenticationActivity.this,
+					GDDiscoverMeActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			finish();
+		} else {
+			Toast.makeText(getApplicationContext(), "invalid credentials",
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	private final OnClickListener onEnterClick = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			// do something when the button is clicked
 
-			TextView username = (TextView) findViewById(R.id.username);
-			TextView password = (TextView) findViewById(R.id.password);
-
-			String usr = username.getText().toString();
-			String pswrd = password.getText().toString();
-
-			Boolean auth = Authenticate.check(usr, pswrd);
-			if (auth) {
-				SharedPreferences prefs = getSharedPreferences("credentials",
-						Context.MODE_WORLD_READABLE);
-				SharedPreferences.Editor editor = prefs.edit();
-				editor.putString("username", usr);
-				editor.putString("password", pswrd);
-				editor.commit();
-				datasource = new MyDataSource(AuthenticationActivity.this);
-				datasource.open();
-				dirdatasource = new DirDataSource(AuthenticationActivity.this);
-				dirdatasource.open();
-				ServerLink.loadPeople(dirdatasource);
-				ServerLink.loadFriends(usr, datasource);
-				ServerLink.loadEvents("saqib01", datasource);
-				// ServerLink.loadEvents("pmerc02", datasource);
-				// ServerLink.loadEvents("pmerc03", datasource);
-				ServerLink.loadNotifs(usr, datasource, dirdatasource);
-				datasource.close();
-				dirdatasource.close();
-				Toast.makeText(getApplicationContext(), "valid credentials!!",
-						Toast.LENGTH_SHORT).show();
-				Intent intent = new Intent(AuthenticationActivity.this,
-						GDDiscoverMeActivity.class);
-				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(intent);
-				finish();
-			} else {
-				Toast.makeText(getApplicationContext(), "invalid credentials",
-						Toast.LENGTH_SHORT).show();
-			}
+			doLogin();
 			
 		}
 	};
